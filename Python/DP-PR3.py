264aee6745fe4cc995605fb783e2a185298b8bf9
Miguel-kjh <miguelangel.kjh@gmail.com>
2018-12-03 21:19:23 +0100

    python tabulation and memorization: los resultados de los tiempos son muy significativos

--- Python/DP-PR3.py
+++ Python/DP-PR3.py
@@ -16,6 +16,26 @@ def optimalStrategyOfGameRecursive(table,arr,i,j):
     (table[i])[j] = max(arr[i] + min(x,y), arr[j] + min(y,z))
     return (table[i])[j]
 
+
+def optimalStrategyOfGameTabulation(arr, n):
+    table = [[0 for i in range(n)]
+             for i in range(n)]
+    for gap in range(n):
+        for j in range(gap, n):
+            i = j - gap
+            x = 0
+            if ((i + 2) <= j):
+                x = table[i + 2][j]
+            y = 0
+            if ((i + 1) <= (j - 1)):
+                y = table[i + 1][j - 1]
+            z = 0
+            if (i <= (j - 2)):
+                z = table[i][j - 2]
+            table[i][j] = max(arr[i] + min(x, y),
+                              arr[j] + min(y, z))
+    return table[0][n - 1]
+
 def llamada_tiempo(fichero,ft):
     iteraciones = 0
     for linea in open(fichero, "r"):
@@ -40,18 +60,34 @@ def llamada_tiempo(fichero,ft):
         ft.write(str(len(arr)) + "--" + str(medida) + "\n")
         print("tiempo: " + str(medida))
 
+def llamada_tiempo_tabulation(fichero,ft):
+    iteraciones = 0
+    for linea in open(fichero, "r"):
+        iteraciones = iteraciones + 1
+        print("################ " +
+              str(iteraciones) +
+              " iteracion ##############")
+        arr = []
+        for palabra in linea.split():
+            arr.append(int(palabra))
+        st = time.time()
+        print(optimalStrategyOfGameTabulation(arr,len(arr)))
+        medida = time.time() - st
+        ft.write(str(len(arr)) + "--" + str(medida) + "\n")
+        print("tiempo: " + str(medida))
 
-    # Tabla comparativa de tiempos(versión primitiva)
-    """
-    elementos | iterativo(tabla) s | recursivo(memorization) s
-    100         0.03                0.0279
-    250         0.186               0.318
-    400         0.5                 0.58
-    550         1.02                1.009
-    700         1.66                1.09
-    850         2.41                1.64
-    1000        3.17                3.39
-    """
+def llamada_funcinamiento_tabulation(fichero):
+    iteraciones = 0
+    for linea in open(fichero, "r"):
+        iteraciones = iteraciones + 1
+        print("################ " +
+              str(iteraciones) +
+              " iteracion ##############")
+        arr = []
+        for palabra in linea.split():arr.append(int(palabra))
+        print("Para" + str(arr))
+        print(optimalStrategyOfGameTabulation(arr,len(arr)))
+
 def llamada_funcinamiento(fichero):
     iteraciones = 0
     for linea in open(fichero, "r"):
@@ -73,6 +109,8 @@ def llamada_funcinamiento(fichero):
         print("Para" + str(arr))
         print(optimalStrategyOfGameRecursive(table, arr, 0, len(arr) - 1))
 
+
+
 if __name__ == '__main__':
     if len(sys.argv) > 1:
         if sys.argv[1] == "-t":
@@ -80,16 +118,28 @@ if __name__ == '__main__':
                 print("------------------------------------------------------- "
                       + str(i - 1) +
                       "Fichero -------------------------------------------------------------------------------------------")
-                ft = open("tiempos.txt", "w+")
+                ft = open("tiempos_Memoritation.txt", "w+")
                 ft.write("##### TIEMPOS " +  str(sys.argv[i]) + " ####\n")
                 llamada_tiempo(sys.argv[i], ft)
+                ft.close()
+        elif sys.argv[1] == "-T":
+            for i in range(2, len(sys.argv)):
+                print("------------------------------------------------------- "
+                      + str(i - 1) +
+                      "Fichero -------------------------------------------------------------------------------------------")
+                ft = open("tiempos_Tabulation.txt", "w+")
+                ft.write("##### TIEMPOS " +  str(sys.argv[i]) + " ####\n")
+                llamada_tiempo_tabulation(sys.argv[i], ft)
                 ft.close()
         else:
             for i in range(1, len(sys.argv)):
                 print("------------------------------------------------------- "
                       + str(i) +
                       "Fichero -------------------------------------------------------------------------------------------")
+                print("Algoritmo por Memoritation")              
                 llamada_funcinamiento(sys.argv[i])
+                print("\nAlgoritmo por Tabulation")
+                llamada_funcinamiento_tabulation(sys.argv[i])
     else:
         print("FALLO EN EL ARGUMENTO")
         exit(1)
