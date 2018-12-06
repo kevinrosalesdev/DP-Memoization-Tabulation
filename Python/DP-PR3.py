b98cb0b3fbb8097fb144f4adc4a0cb983840c51d
Kevindx98 <kevin6bepc@gmail.com>
2018-12-06 13:45:44 +0000

    Corrección de títulos de métodos y ficheros de Python

--- Python/DP-PR3.py
+++ Python/DP-PR3.py
@@ -3,14 +3,14 @@ import sys
 import time
 
 
-def optimalStrategyOfGameRecursive(table,arr,i,j):
+def optimalStrategyOfGameMemoization(table,arr,i,j):
     if(i == j):
         return arr[i]
-    if((i+2) <= j): x = optimalStrategyOfGameRecursive(table, arr, i+2, j)
+    if((i+2) <= j): x = optimalStrategyOfGameMemoization(table, arr, i+2, j)
     else: x = 0
-    if ((i + 1) <= j-1): y = optimalStrategyOfGameRecursive(table, arr, i + 1, j-1)
+    if ((i + 1) <= j-1): y = optimalStrategyOfGameMemoization(table, arr, i + 1, j-1)
     else: y = 0
-    if (i <= (j-2)): z = optimalStrategyOfGameRecursive(table, arr, i, j-2)
+    if (i <= (j-2)): z = optimalStrategyOfGameMemoization(table, arr, i, j-2)
     else: z = 0
 
     (table[i])[j] = max(arr[i] + min(x,y), arr[j] + min(y,z))
@@ -36,7 +36,7 @@ def optimalStrategyOfGameTabulation(arr, n):
                               arr[j] + min(y, z))
     return table[0][n - 1]
 
-def llamada_tiempo(fichero,ft):
+def llamada_tiempo_memoization(fichero,ft):
     iteraciones = 0
     for linea in open(fichero, "r"):
         iteraciones = iteraciones + 1
@@ -55,7 +55,7 @@ def llamada_tiempo(fichero,ft):
                 # asignación para la lista
                 table[i].append(0)
         st = time.time()
-        print(optimalStrategyOfGameRecursive(table, arr, 0, len(arr) - 1))
+        print(optimalStrategyOfGameMemoization(table, arr, 0, len(arr) - 1))
         medida = time.time() - st
         ft.write(str(len(arr)) + "--" + str(medida) + "\n")
         print("tiempo: " + str(medida))
@@ -88,7 +88,7 @@ def llamada_funcionamiento_tabulation(fichero):
         print("Para" + str(arr))
         print(optimalStrategyOfGameTabulation(arr,len(arr)))
 
-def llamada_funcionamiento(fichero):
+def llamada_funcionamiento_memoization(fichero):
     iteraciones = 0
     for linea in open(fichero, "r"):
         iteraciones = iteraciones + 1
@@ -107,7 +107,7 @@ def llamada_funcionamiento(fichero):
                 # asignación para la lista
                 table[i].append(0)
         print("Para" + str(arr))
-        print(optimalStrategyOfGameRecursive(table, arr, 0, len(arr) - 1))
+        print(optimalStrategyOfGameMemoization(table, arr, 0, len(arr) - 1))
 
 
 
@@ -118,9 +118,9 @@ if __name__ == '__main__':
                 print("------------------------------------------------------- "
                       + str(i - 1) +
                       "Fichero -------------------------------------------------------------------------------------------")
-                ft = open("tiempos_Memoritation.txt", "w+")
+                ft = open("tiempos_Memoization.txt", "w+")
                 ft.write("##### TIEMPOS " +  str(sys.argv[i]) + " ####\n")
-                llamada_tiempo(sys.argv[i], ft)
+                llamada_tiempo_memoization(sys.argv[i], ft)
                 ft.close()
         elif sys.argv[1] == "-T":
             for i in range(2, len(sys.argv)):
@@ -137,7 +137,7 @@ if __name__ == '__main__':
                       + str(i) +
                       "Fichero -------------------------------------------------------------------------------------------")
                 print("Algoritmo por Memoritation")              
-                llamada_funcionamiento(sys.argv[i])
+                llamada_funcionamiento_memoization(sys.argv[i])
                 print("\nAlgoritmo por Tabulation")
                 llamada_funcionamiento_tabulation(sys.argv[i])
     else:
